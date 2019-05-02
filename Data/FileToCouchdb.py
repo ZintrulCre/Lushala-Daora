e70320af0ae82649eb5b872ff8ba8cc92e59367b
zintrulcre <zintrulcre@gmail.com>
2019-05-02 19:22:55 +1000

    Update FileToCouchdb.py

--- Data/FileToCouchdb.py
+++ Data/FileToCouchdb.py
@@ -1,10 +1,33 @@
 import os
+import sys
 import json
 import time
-os.system("pip install requests")
-import requests
 
-with open('Data/test.json') as file:
+if len(sys.argv) == 2 and sys.argv[1] == '-h':
+    print("python3 FileToCouchdb.py <ip> <port> <database> <user> <password> <file>")
+    sys.exit()
+elif len(sys.argv) != 7:
+    print("Wrong arguments!")
+    sys.exit()
+
+os.system("pip install couchdb")
+import couchdb
+
+ip = sys.argv[1]
+port = sys.argv[2]
+database = sys.argv[3]
+user = sys.argv[4]
+password = sys.argv[5]
+file = sys.argv[6]
+
+server = couchdb.Server("http://" + user + ":" + password + "@" + ip + ":" + port + "/")
+# server = couchdb.Server("http://" + ip + ":" + port + "/")
+if database in server:
+    db = server[database]
+else:
+    db = server.create(database)
+
+with open(file) as file:
     file.readline()
     id = 1
     for line in file:
@@ -13,17 +36,6 @@ with open('Data/test.json') as file:
         line = line.strip().strip(',')
         content = json.loads(line)
         text = content['doc']['text']
-        data = {}
-        data['id'] = str(id)
-        data['text'] = text
-        data = json.dumps(data)
-        # print(data)
-        while True:
-            response = requests.put("http://172.26.37.212:5984/melb/" + str(id), data=data)
-            print(response.text)
-            if "ok" in response.text:
-                break
-        # command = "curl -u admin:password -X PUT http://172.26.37.212:5984/melbourne_db/" + str(id) + " -d '" + data + "'"
-        # print(command)
-        # os.system(command)
+        db[str(id)] = {'text':text}
+        print(db[str(id)])
         id += 1
