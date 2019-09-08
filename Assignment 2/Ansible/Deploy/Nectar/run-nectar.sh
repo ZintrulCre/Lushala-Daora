d7752e8fdef0f85c2f73579f64d979b0481a5b44
zintrulcre <zintrulcre@gmail.com>
2019-09-08 11:18:17 +1000

    merge

--- Assignment 2/Ansible/Deploy/Nectar/run-nectar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./openrco.sh; ansible-playbook --ask-become-pass nectar.yaml
--- Assignment 2/Ansible/Docker/run-Docker.sh
+++
@@ -1,3 +0,0 @@
-sudo . ./openrc.sh; ansible-playbook -b -i hosts -u ubuntu --key-file=~/.ssh/id_rsa main.yaml
-
-
